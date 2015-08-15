df5c151cae3a6d89ba4dc3b5ea2ecf67317cbff8
vertraut <onlinemovement@gmail.com>
2015-08-15 23:37:47 +0300

    Extracted FileHelper from PhoneBook.

--- src/PhoneBook.java
+++ src/PhoneBook.java
@@ -1,31 +1,28 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.util.Scanner;
+
+import static helpers.FileHelper.readFile;
 
 
 public class PhoneBook {
 
-    static String fileName = "src\\phoneBook.txt";
 
     public static void main(String[] args) throws Exception {
         PhoneBook book = new PhoneBook();
-
-        book.maneMenu();
+        book.openMainMenu();
     }
 
 
-    void maneMenu() throws Exception { //вывод меню
+    private void openMainMenu() throws Exception { //вывод меню
         while (true) {
             System.out.println("-== Телефонная книга ==-");
             System.out.println("Выберете действие:");
             System.out.println("1. Просмотр контактов");
             System.out.println("2. Добавление нового контакта");
-            switchMainMenu(reader());
+            switchMainMenu(read());
         }
     }
 
-    int reader() {//чтение консоли
+    private int read() {//чтение консоли
         Scanner in = new Scanner(System.in);
         int a = 0;
         try {
@@ -39,13 +36,13 @@ public class PhoneBook {
         return a;
     }
 
-    void switchMainMenu(int choice) throws Exception {//переход по основному меню
+    private void switchMainMenu(int choice) throws Exception {//переход по основному меню
         switch (choice) {
             case 1:
-                contactListView();
+                openContactListViewMenu();
                 break;
             case 2:
-                contactAdd();
+                addContact();
                 break;
             default:
                 System.out.println("!!!Неверный ввод!!!");
@@ -54,19 +51,14 @@ public class PhoneBook {
         }
     }
 
-
-    void contactListView() throws Exception {
-        ContactListViewMenu();
-    }
-
-    void contactAdd() {
+    private void addContact() {
         System.out.println("-== Добавление нового контакта ==-");
     }
 
-    void ContactListViewMenu() throws Exception { //вывод меню
+    private void openContactListViewMenu() throws Exception { //вывод меню
         while (true) {
             System.out.println("-== Список контактов ==-");
-            fileRead();//вывод всех контактов
+            readFile();//вывод всех контактов
             System.out.println("Выберете действие:");
             System.out.println("1. Просмотреть контакт");
             System.out.println("2. Удалить контакт");
@@ -74,20 +66,20 @@ public class PhoneBook {
             System.out.println("4. Поиск по контактам");
             System.out.println("5. В главное меню");
 
-            switchContactListViewMenu(reader());
+            switchContactListViewMenu(read());
         }
     }
 
-    void switchContactListViewMenu(int choice) throws Exception {//переход по меню просмотра телефонной книги
+    private void switchContactListViewMenu(int choice) throws Exception {//переход по меню просмотра телефонной книги
         switch (choice) {
             case 1:
-                contactListView();
+                openContactListViewMenu();
                 break;
             case 2:
-                contactAdd();
+                addContact();
                 break;
             case 5:
-                maneMenu();
+                openMainMenu();
 
                 break;
             default:
@@ -97,25 +89,6 @@ public class PhoneBook {
         }
     }
 
-
-    static void fileRead() throws Exception { // чтение файла
-        int num = 1;
-        FileReader fr = new FileReader(fileName);
-        BufferedReader b = new BufferedReader(fr);
-        String str;
-        while ((str = b.readLine()) != null) {
-            System.out.println(num + ". " + str);
-            num++;
-
-        }
-        fr.close();
-    }
-
-    void fileWrite(String firstName, String lastName, String number) throws Exception { // Добавление номера в файл
-        FileWriter fw = new FileWriter(fileName, true);
-        fw.write(firstName + " " + lastName + " " + number + "\n");
-        fw.close();
-    }
 
 }
 
---
+++ src/helpers/FileHelper.java
@@ -0,0 +1,59 @@
+package helpers;
+
+import java.io.*;
+
+public class FileHelper {
+
+    private static final String FILE_NAME = "src\\phoneBook.txt";
+
+    public static void readFile() { // чтение файла
+        int num = 1;
+        FileReader fr;
+        BufferedReader b = null;
+        try {
+            fr = new FileReader(FILE_NAME);
+            b = new BufferedReader(fr);
+            String str;
+            while ((str = b.readLine()) != null) {
+                System.out.println(num + ". " + str);
+                num++;
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (b != null) {
+                    b.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void writeFile(String firstName, String lastName, String number) { // Добавление
+        // номера в файл
+        //TODO: add buffer
+        FileWriter fw;
+        BufferedWriter bw = null;
+        try {
+            fw = new FileWriter(FILE_NAME, true);
+            bw = new BufferedWriter(fw);
+            fw.write(firstName + " " + lastName + " " + number + "\n");
+            fw.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            try {
+                if (bw != null) {
+                    bw.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+}
+
